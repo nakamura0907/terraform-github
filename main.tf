a023a9f65646bbc9741940b82079602b6be78f35
nakamura0907 <nakamura0907@users.noreply.github.com>
2023-12-30 16:09:54 +0900

    github_repositoryリソースの設定

--- main.tf
+++ main.tf
@@ -9,10 +9,19 @@ terraform {
 
 provider "github" {
   # Authenticating with the Github provider using Github CLI.
+  # See https://registry.terraform.io/providers/integrations/github/latest/docs#github-cli
 }
 
 resource "github_repository" "example" {
   name        = "example-repo"
   description = "An example GitHub repository"
   visibility  = "public"
+
+  ## Features
+  has_issues = true
+
+  ## Pull Requests
+  allow_rebase_merge = false
+  allow_update_branch = true
+  delete_branch_on_merge = true
 }
